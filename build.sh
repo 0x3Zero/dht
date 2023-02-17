9da13c49914e590e2b3d6d53af2495051ff87365
saifiikmal <saifi.ikmal@gmail.com>
2023-02-17 10:35:44 +0800

    Added builtins

--- build.sh
+++ build.sh
@@ -16,4 +16,6 @@ marine aqua artifacts/dht.wasm -s dht -i Dht > ./aqua/dht.aqua
 wget https://github.com/fluencelabs/sqlite/releases/download/v0.15.0_w/sqlite3.wasm
 mv sqlite3.wasm artifacts/
 
+cp -f artifacts/*.wasm builtins/
+
 RUST_LOG="info" mrepl --quiet Config.toml
